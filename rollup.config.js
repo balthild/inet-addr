d45b564f03e9c711e33ceea636bd377d42ef6b51
Balthild <ibalthild@gmail.com>
2022-06-10 10:46:30 +0800

    Generate .d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,44 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import dts from "rollup-plugin-dts";
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/lib.ts',
-		output: {
-			name: 'howLongUntilLunch',
-			file: pkg.browser,
-			format: 'umd',
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-			typescript(),
-		],
-	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	{
-		input: 'src/lib.ts',
-		external: ['big-integer'],
-		plugins: [
-			typescript(),
-		],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' },
-		],
-	},
+    // Browser-friendly UMD build.
+    {
+        input: 'src/lib.ts',
+        output: {
+            name: 'howLongUntilLunch',
+            file: pkg.browser,
+            format: 'umd',
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript(),
+        ],
+    },
+    // CommonJS (for Node) and ES module (for bundlers) build.
+    {
+        input: 'src/lib.ts',
+        external: ['big-integer'],
+        plugins: [
+            typescript(),
+        ],
+        output: [
+            { file: pkg.main, format: 'cjs' },
+            { file: pkg.module, format: 'es' },
+        ],
+    },
+    // TypeScript type definitions.
+    {
+        input: 'src/lib.ts',
+        output: [
+            { file: pkg.types, format: 'es' },
+        ],
+        plugins: [
+            dts(),
+        ],
+    },
 ];
